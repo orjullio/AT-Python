d54f1c64e2798a7be53534937983a48968d51b48
Rodrigo Júlio <rjullio@outlook.com>
2018-12-20 18:14:17 -0200

    Alterações nos exercícios 4, 5, 6, 7, 8

--- 03.py
+++ 03.py
@@ -4,27 +4,31 @@
 # Ordene decrescentemente esta estrutura pelo valor da quantidade de bytes ocupada em disco (pode usar as funções sort
 # ou sorted);
 # gere um arquivo texto com os valores desta estrutura ordenados.
-import os
+import os, sys
 
-print('Exerciio 3\n')
-usuario = str(input('Olá, por favor digite seu nome:\n'))
-print('Obrigado {}'.format(usuario))
-diretorio = str(input('Agora por favor, digite um nome para o diretório:\n'))
+def main():
+    print('Exerciio 3\n')
+    usuario = str(input('Olá, por favor digite seu nome:\n'))
+    print('Obrigado {}'.format(usuario))
+    diretorio = str(input('Agora por favor, digite um nome para o diretório:\n'))
+    try:
+        os.mkdir(diretorio)
+    except FileExistsError:
+        print('Já existe um diretório com esse nome: ')
+    except Exception as e:
+        print(str(e))
+    arquivos = os.listdir(diretorio)
+    dados = []
+    for arquivo in arquivos:
+        p = os.path.join(diretorio, arquivo)
+        dado = os.stat(p)
+        dados.append({'nome: ': arquivo, 'tamanho: ': dado.st_size})
 
-try:
-    os.mkdir(diretorio)
-except FileExistsError:
-    print('Já existe um diretório com esse nome: ')
-except Exception as e:
-    print(str(e))
-
-dir_path = 'C:/Users/Rodrigo/PycharmProjects/DPR-AT/{}/'.format(str(diretorio))
-files = os.listdir(dir_path)
-for f in files:
-    f_path = os.path.join(dir_path,f)
-    f_size = os.path.getsize(f_path)
-    f_size_kb = f_size/1024 # obter resultado em kB
-    print(f_path, f_size_kb)
+    dados.sort(key=lambda l: l['tamanho'])
+    print(dados)
 
 
+
+if __name__ == '__main__':
+    main()
 
